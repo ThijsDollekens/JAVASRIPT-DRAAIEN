d4ab25207334a8e30098819e98352f093d2c59e7
ThijsDollekens <87063168+ThijsDollekens@users.noreply.github.com>
2021-08-01 13:40:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten")
+
+console.log("Dit is een grote som.")
+/*let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:", division);*/
+
